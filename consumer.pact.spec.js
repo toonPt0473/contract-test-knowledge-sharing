96e010ca013f1c61854edbd7337b71d59a71b7f7
Paitoon Arayasatjapong <paitoona@bedrockanalytics.ai>
2024-12-11 02:05:34 +0700

    feat: add order test case to product

--- consumer.pact.spec.js
+++ consumer.pact.spec.js
@@ -1,10 +1,11 @@
 // (1) Import the pact library and matching methods
 const { Pact } = require("@pact-foundation/pact");
-const { ProductApiClient } = require("./api");
+const { ProductApiClient, OrderClient } = require("./api");
 const { Product } = require("./product");
 const { Matchers } = require("@pact-foundation/pact");
 const { like, regex } = Matchers;
 const chai = require("chai");
+const { Order } = require("./order");
 const expect = chai.expect;
 
 // (2) Configure our Pact library
@@ -52,6 +53,41 @@ describe("Products API test", () => {
     // (6) Assert that we got the expected response
     expect(product).to.deep.equal(
       new Product(10, "Margharita", "pizza", expectedProduct)
+    );
+  });
+
+  it("get order by ID", async () => {
+    // (4) Arrange
+    const expectedOrder = {
+      id: 10,
+      type: "pizza",
+      name: "Margharita",
+      active: true,
+    };
+
+    await mockProvider.addInteraction({
+      state: "a order with ID 10 exists",
+      uponReceiving: "a request to get a order",
+      withRequest: {
+        method: "GET",
+        path: "/orders/10",
+      },
+      willRespondWith: {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: like(expectedOrder),
+      },
+    });
+
+    // (5) Act
+    const api = new OrderClient(mockProvider.mockService.baseUrl);
+    const order = await api.getOrder(10);
+
+    // (6) Assert that we got the expected response
+    expect(order).to.deep.equal(
+      new Order(10, "Margharita", "pizza", expectedOrder)
     );
   });
 });
